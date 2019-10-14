1caffb73de884aa0c19191fad8e18fef76267ba8
Yurij2015 <nabor_2008@i.ua>
2019-10-14 08:29:45 +0300

    add access to controllers cust orders sales services

--- controllers/ServicesController.php
+++ controllers/ServicesController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Services;
 use app\models\ServicesSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,15 @@ class ServicesController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
